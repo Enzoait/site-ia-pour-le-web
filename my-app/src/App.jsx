6d77ef448aebe0fb3e9761bdb90b59bcc73c0855
Enzoait <pcenzox25@gmail.com>
2025-07-01 10:37:02 +0200

    Exportation du model pth en onnx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as ort from 'onnxruntime-web';
+
 
 function App() {
   const [count, setCount] = useState(0)
